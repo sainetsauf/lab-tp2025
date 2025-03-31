fcf92d6a02a8f23856e32b66d5c9c9b84847553b
Maria <airaaaam.msh@gmail.com>
2025-03-31 19:45:23 +0300

    Fixed the output for T2

--- shvetsova.maria/T2/DataStruct.cpp
+++ shvetsova.maria/T2/DataStruct.cpp
@@ -66,8 +66,8 @@ namespace nspace{
             return out;
         }
         iofmtguard fmtguard(out);
-        out << "(:key1 " << std::fixed << std::setprecision(1) << dest.key1 << "d ";
-        out << ":key2 " << dest.key2 << "ull ";
+        out << "(:key1 " << std::fixed << std::setprecision(1) << dest.key1 << "d";
+        out << ":key2 " << dest.key2 << "ull";
         out << ":key3 \"" << dest.key3 << "\":)";
         return out;
     }
