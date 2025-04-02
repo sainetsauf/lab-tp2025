b1cae62fc92fa47d6bf03f900bf0bf34685e742f
F0LY <isisu7337@gmail.com>
2025-04-02 22:53:35 +0300

    попытка 4.main выводит имя и фамилию

--- saraev.gordey/T0/main.cpp
+++ saraev.gordey/T0/main.cpp
@@ -1,1 +1,6 @@
-#include <iostream>int main() {    std::cout << "saraev.gordey" << std::endl;    return 0;}
+#include <iostream>
+
+int main() {
+    std::cout << "saraev.gordey" << std::endl;
+    return 0;
+}
