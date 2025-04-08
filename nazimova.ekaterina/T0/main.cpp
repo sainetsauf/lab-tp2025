5e51e6ca82c857654e6364e9c475f06af8022e19
UwUshkin <ekaterinanazimova07@gmail.com>
2025-04-08 23:44:13 +0300

    add innitial commit for T0

---
+++ nazimova.ekaterina/T0/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    std::string surname = "Nazimova";
+    std::string name = "Ekaterina";
+
+    // Преобразование в нижний регистр
+    for (char& c : surname) c = std::tolower(c);
+    for (char& c : name) c = std::tolower(c);
+
+    std::cout << surname << "." << name << std::endl;
+
+    return 0;
+}
